07eb7a43bf360de4b8f4f6ec6c4d095d17215d7c
NOPE <kyb0809tg@naver.com>
2022-12-04 22:58:05 +0900

    fix the undefined behavior problem

--- comparesize.cpp
+++ comparesize.cpp
@@ -16,10 +16,37 @@ char comparesize(int a, int b)
 	return compare[k];
 }
 
+int comparesizeBig(int a, int b)
+{
+	int compare[3]={b,b,a};
+	int k=0;
+	k=a-b;
+	while((k!=1 && k!=-1)&&k!=0)
+	{
+		k/=2;
+	}
+	cout<<"left : "<<a<<", right : "<<b<<", big one : "<<compare[k+1]<<endl;
+	return compare[k+1];
+}
+
+int comparesizeSmall(int a, int b)
+{
+	int compare[3]={a,b,b};
+	int k=0;
+	k=a-b;
+	while((k!=1 && k!=-1)&&k!=0)
+	{
+		k/=2;
+	}
+	cout<<"left : "<<a<<", right : "<<b<<", small one : "<<compare[k+1]<<endl;
+	return compare[k+1];
+}
+
 void comparetest(void)
 {
 	char result;
-	result = comparesize(rand()%100, rand()%100);
+	result = comparesizeBig(rand()%100, rand()%100);
+	result = comparesizeSmall(rand()%100, rand()%100);
 }
 
 int main(void)
